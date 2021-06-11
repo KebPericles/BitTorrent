8830bf12fcc5ddc65c5f50123d0c9a320e4fedb0
Alan Rosas Palacios <43190667+trimaxlan98@users.noreply.github.com>
2021-06-10 22:13:32 -0500

    Ultima actualización

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BitTorrent
  Proyecto final Sistemas Distribuidos Programa
+
+'javac ./*.java ./util/lib/*.java ./util/bencode/*.java'
+
+Seeder: java BitClient -z slow -x seed -w 7777 hubble.jpg.torrent
+java BitClient -z slow -p localhost:7777 -w 6666 -s ./test/downloads/hubbleChameleon.jpg hubble.jpg.torrent
+java BitClient -z slow -w 7777 -p 127.0.0.1:7777 -p 127.0.0.1:6666 -s ./test/downloads/hubbleDolphin.jpg hubble.jpg.torrent 
---
+++ src/util/bencode/BObject.java
@@ -0,0 +1,24 @@
+/* BObject.java:  interface for a decoded Bencode object */
+/* Christopher Chute */
+
+/* Reference: adapted from @frazboyz implementation on BitBucket */
+/* https://bitbucket.org/frazboyz/bencoder */
+
+package util.bencode;
+
+public interface BObject {
+    public enum BObjectType {
+        BDICT,
+        BSTRING,
+        BNUMBER,
+        BLIST
+    }
+    /* encode:  convert BObject to Bencoded string representation */
+    public String encode();
+
+    /* print:  produce a human-readable string */
+    public String print();
+
+    /* getType:  returns type of BObject */
+    public BObjectType getType();
+}
